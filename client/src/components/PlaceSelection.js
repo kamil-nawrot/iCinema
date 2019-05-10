91f7a85a558a34bdaa915a8106286010a8bebf98
Ewa <ewa.szczygielska@onet.pl>
2019-05-10 20:26:22 +0200

    place selection

--- client/src/components/PlaceSelection.js
+++ client/src/components/PlaceSelection.js
@@ -4,8 +4,28 @@ import { selectPlace } from '../actions';
 
 class PlaceSelection extends React.Component {
     render() {
-        return (console.log('hello'));
-    }
-}
+                return ( <
+                    div className = 'seats-con' > {
+                        this.props.seat.map(row =>
+                            <
+                            div className = {
+                                this.props.choosen.indexOf(row) > -1 ? 'choosen' : this.props.reserved.indexOf(row) > -1 ? 'reserved' : 'available'
+                            }
+                            key = {
+                                row
+                            }
+                            onClick = {
+                                e => this.onClickSeat(row)
+                            } > {
+                                row
+                            } < /div>)} < /
+                            div >
+                        )
+                    }
+
+                    onClickSeat(seat) {
+                        this.props.onClickData(seat)
+                    }
+                }
 
 export default connect(null)(PlaceSelection);
