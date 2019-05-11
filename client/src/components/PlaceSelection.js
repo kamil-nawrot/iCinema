ecf74e95e501976ed5f15001da913458434a0608
Ewa <ewa.szczygielska@onet.pl>
2019-05-11 19:15:27 +0200

    seats prototype

--- client/src/components/PlaceSelection.js
+++ client/src/components/PlaceSelection.js
@@ -10,7 +10,7 @@ class PlaceSelection extends React.Component
                 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10',
                 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10',
                 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10',
-                'D1', 'D2', 'D3', 'D4', 'D5', 'C6', 'C7', 'C8', 'C9', 'C10',
+                'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10',
                 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9', 'E10',
                 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10',
             ],
@@ -18,7 +18,7 @@ class PlaceSelection extends React.Component
                 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10',
                 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10',
                 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10',
-                'D1', 'D2', 'D3', 'D4', 'D5', 'C6', 'C7', 'C8', 'C9', 'C10',
+                'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10',
                 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9', 'E10',
                 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8'
             ],
@@ -57,8 +57,8 @@ class PlaceSelection extends React.Component
 
     render() {
         return (
-            <div> 
-                <h1> Select your place! </h1> 
+            <div className = "container1"> 
+                <h2> Select your place! </h2>
                 <ChairGrid place={this.state.place}
                           available={this.state.placeAvailable}
                           reserved={this.state.placeReserved}
@@ -78,7 +78,7 @@ class ChairGrid extends React.Component
                             {this.props.place.map(row => 
                                 <div className={this.props.checked.indexOf(row) > -1 ?'chairchecked' :
                                                this.props.available.indexOf(row) > -1 ?'chairavailable' :'chairreserved'}
-                                    //key={row}
+                                    key={row}
                                     onClick={ e => this.onClickPlace(row)}>
                                     <span> {row} </span>
                                 </div>)
@@ -106,6 +106,12 @@ class CheckedList extends React.Component
                     </span>)}  
                 </div> 
                 <h4>The number of tickets: {this.props.checked.length} </h4>
+                <div className='legend'>
+                    <div>Legend:</div>
+                    <span className = 'chairchecked'> Your Chairs </span>
+                    <span className = 'chairavailable'> Available </span>
+                    <span className = 'chairreserved'> Reserved </span>
+                </div>
             </div>
         )
     }
