3050f963f384611ba796344f936c84a03a007c49
Adam <pelswick100@wp.pl>
2019-05-15 01:59:35 +0200

    start writing of confirmation page

--- client/src/components/Confirmation.js
+++ client/src/components/Confirmation.js
@@ -8,7 +8,23 @@ class Confirmation extends React.Component
 {
     render() {
         return (
-            <div className="container">
+            <div className = "container" id = "personal-info-box" >
+            <hr></hr>
+            <h1>Confirm your reservation</h1>
+            <hr></hr>
+            <h2>Movie info</h2>
+            <hr></hr>
+            <p>Movie:</p>
+            <p>Time: </p>
+            <p>Ticket: </p>
+            <p>Seat: </p>
+            <hr></hr>
+            <h2>Personal info:</h2>
+            <hr></hr>
+            <p>First name: </p>
+            <p>Last name: </p>
+            <p>E-mail: </p>
+            <p>Phone: </p>
             <NavLink className = "arrowleft" to = "/personal-details"> <i className = "fas fa-angle-double-left"> </i></NavLink>
                 <button className="option-button" onClick={() => {
                     this.props.confirmBooking(this.props);
