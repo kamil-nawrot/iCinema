88a11d6c5d4abcf083ec7dcacfab82ad58e24145
Kamil Nawrot <kamilnawrot98@gmail.com>
2019-05-14 19:46:53 +0200

    fixed navigation issues

--- client/src/components/RegionSelection.js
+++ client/src/components/RegionSelection.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { selectRegion } from '../actions';
+import { selectRegion, selectMovie, selectShowing, selectSeats } from '../actions';
 
 class RegionSelection extends React.Component
 {
+    componentDidUpdate(prevProps) {
+        if (this.props.region !== prevProps.region) {
+            this.props.selectMovie('');
+            this.props.selectShowing(null);
+            this.props.selectSeats([]);
+        }
+    }
+
     renderOptions() {
         const locales = [['PL', 'pl-PL', 'Poland'], 
                          ['us', 'en-US', 'United States'],
@@ -16,6 +24,7 @@ class RegionSelection extends React.Component
             return (
                 <button key={locale[1]} className={`option-button ${this.props.region.region === locale[0] ? 'active' : '' }`} onClick={() => {
                     this.props.selectRegion(locale[0], locale[1]);
+                    
                 }}>{locale[2]}</button>
             );
         })
@@ -24,7 +33,7 @@ class RegionSelection extends React.Component
     render() {
         return (
             <div className="container" style={{backgroundColor: 'rgba(34,34,34,0.3)'}}>
-                <div className="option">
+                <div className="option" style={{flexWrap: "nowrap"}}>
                     <h2> Welcome to our online cinema tickets booking app </h2>
                     <h3> Please select your region and language and we can proceed </h3>
                     <div className="option-wrapper">
@@ -40,4 +49,4 @@ const mapStateToProps = state => {
     return { region: state.region };
 }
 
-export default connect(mapStateToProps, { selectRegion })(RegionSelection);
+export default connect(mapStateToProps, { selectRegion, selectMovie, selectShowing, selectSeats })(RegionSelection);
