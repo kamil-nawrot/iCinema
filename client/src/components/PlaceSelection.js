acd1a99f5c1c567fd9b763947dac92a81a68adf5
Ewa <ewa.szczygielska@onet.pl>
2019-05-15 21:10:29 +0200

    changes place selection

--- client/src/components/PlaceSelection.js
+++ client/src/components/PlaceSelection.js
@@ -9,7 +9,7 @@ class PlaceSelection extends React.Component
     constructor(props) {
         super(props);
         this.props.findBookings(this.props.selectedMovie.id, this.props.selectedShowing);
-        console.log(this.props.foundBookings);
+        //console.log(this.props.foundBookings);
         this.state = {
             place: [
                 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10',
