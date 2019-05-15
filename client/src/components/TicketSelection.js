8b864d2e49f8cfbb77e5652d99f3ac24f7ca2c8c
Kacper <kacperszymula@wp.pl>
2019-05-15 19:40:34 +0200

    tickets-part2

--- client/src/components/TicketSelection.js
+++ client/src/components/TicketSelection.js
@@ -7,6 +7,7 @@ import {NavLink} from 'react-router-dom';
 import { selectRegion, selectMovie, selectShowing } from '../actions';
 
 class TicketSelection extends React.Component {
+
     state = {
             juniorNumber: 0,
             studentNumber: 0,
@@ -16,25 +17,48 @@ class TicketSelection extends React.Component {
 
     onFormSubmit = event =>{
         event.preventDefault();
+        console.log(this.state.juniorNumber);
     }
 
+    currency(props) {
+        if (props.region === 'PL'){
+            return 'PLN'
+        } else if(props.region === 'us'){
+            return 'USD'
+        } else if (props.region === 'gb' ){
+            return "GPB"
+        } else if (props.region === 'fr' ){
+            return "EUR"
+        }else if (props.region === 'es' ){
+            return"EUR"
+        }else if (props.region === 'de' ){
+            return "EUR"
+        }else {
+            return ''
+        }
+
+    };
+    
 
     renderTickets(){
+        
         console.log(this.props.region);
+        console.log(this.state.juniorNumber);
         return(
             <div className="container" id="tickets-box" style={{backgroundColor: 'rgba(34,34,34,0.3)'}}>
                 <h1>Choose your tickets here.</h1>
                 <form id="tickets-info" onSubmit={(e) => this.onFormSubmit(e)} >
                     <div className="ticket-field">
-                        <label>Junior Tickets Number</label>
+                        <label>Junior Tickets (5 waluta) {this.props.region.region}<currency region = {this.props.region}/></label>
                         <NumericInput id="numeric-input"
                             min={0} 
                             max={20} 
                             value={this.state.juniorNumber}
+                            onChange={this._handleUpdate} 
                             />
                     </div>
                     <div className="ticket-field">
-                        <label>Student Tickets Number</label>
+                        <label>Student Tickets (5 waluta) </label>
                         <NumericInput id="numeric-input"
                             min={0} 
                             max={20} 
@@ -42,7 +66,7 @@ class TicketSelection extends React.Component {
                             />
                     </div>
                     <div className="ticket-field">
-                        <label>Normal Tickets Number</label>
+                        <label>Normal Tickets (10 waluta)</label>
                         <NumericInput id="numeric-input" 
                             min={0} 
                             max={20} 
@@ -50,12 +74,16 @@ class TicketSelection extends React.Component {
                             />
                     </div>
                     <div className="ticket-field">
-                        <label>Senior Tickets Number</label>
+                        <label>Senior Tickets (5 waluta)</label>
                         <NumericInput id="numeric-input"
                             min={0} 
                             max={20} 
-                            value={this.state.juniorNumber}
+                            value={this.state.seniorNumber}
                             />
+                    </div>
+                    <div className="sum-field">
+                        <label>Full price:</label>
+                        <label id = "price">tu kwota i waluta</label>
                     </div>
                     <button className="nav-button" id="confirm-btn" style={{width: "50%"}}>CONFIRM</button>
                 </form>
